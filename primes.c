b7f9a22f57f576fdfa61a4fb3614661dcc43cd9b
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2021-12-25 21:36:03 +0900

    Create primes.c

---
+++ primes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void primes(long n) {
+  long i;
+  long j;
+  bool *numbers = malloc((n+1) * sizeof(bool));
+  
+  numbers[2] = true;
+  printf("%d ", 2);
+
+  for(i = 3; i <= n; i+=2) {
+    if (numbers[i]) { continue; }
+    printf("%ld ", i);
+    for(j = i*2; j <= n; j+=i) {
+      numbers[j] = true;
+    }
+  }
+  free(numbers);
+  printf("\n");
+}
+
+int main(int argc, char **argv) {
+  if(argc!=2) {
+    return 1;
+  }
+  long n = atoi(argv[1]);
+  if(argc<=1) {
+    return 1;
+  }
+  primes(n);
+  return 0;
+}
